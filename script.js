4943da94f287b20bb5aa30eb5eaa02028e1f4b75
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2021-07-02 14:34:18 +0530

    final commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var outputText = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
+var body = document.querySelector("#main");
 
-console.log(outputText);
-console.log(color1);
-console.log(color2);
+function setGradient(){
+    body.style.background = "linear-gradient(to right, " + color1.value+", " + color2.value+")";
+    outputText.textContent = body.style.background+";";
+}
+
+
+color1.addEventListener("input", setGradient)
+
+color2.addEventListener("input", setGradient);
 
